9a39c8902a779815ca1cd791600f901a62412d96
IvanTertov4 <71034268+IvanTertov4@users.noreply.github.com>
2021-09-16 22:57:11 +0300

    вторая попытка исправить конфликт веток

--- src/services/constants.js
+++ src/services/constants.js
@@ -5,11 +5,10 @@ const userRanks = {
   movieBuff: 'Movie buff',
 };
 const CARD_COUNT = 5;
-const ESC_CODE = 'Escape';
 const CARD_TOP_COUNT = 2;
-const GENERATION_CARD_COUNT = 40;
+const GENERATION_CARD_COUNT = 125;
 const COMMENT_COUNT = 75;
 const FAN_LIMIT = 11;
 const MOVIE_BUFF_LIMIT = 21;
 
-export {emotions, CARD_COUNT, CARD_TOP_COUNT, GENERATION_CARD_COUNT, COMMENT_COUNT, userRanks, ESC_CODE, FAN_LIMIT, MOVIE_BUFF_LIMIT};
+export {emotions, CARD_COUNT, CARD_TOP_COUNT, GENERATION_CARD_COUNT, COMMENT_COUNT, userRanks, FAN_LIMIT, MOVIE_BUFF_LIMIT};
